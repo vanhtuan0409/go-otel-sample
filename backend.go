87e55fedef5561a17758ae80fcbe436a3ebf7ce8
Tuan Vuong <vanhtuan0409@gmail.com>
2022-11-29 10:03:20 +0700

    add server

--- backend.go
+++ backend.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"sync"
+
+	"github.com/labstack/echo/v4"
 )
 
 func runBackend(ctx context.Context, wg *sync.WaitGroup) {
@@ -12,5 +16,31 @@ func runBackend(ctx context.Context, wg *sync.WaitGroup) {
 		wg.Done()
 	}()
 
-	log.Println("Backend started")
+	e := echo.New()
+	e.HideBanner = true
+	go func() {
+		<-ctx.Done()
+		e.Shutdown(context.Background())
+	}()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "backend")
+	})
+	e.GET("/echo", echoHandler)
+
+	log.Println("Backend starting up")
+	addr := fmt.Sprintf(":%d", backendPort)
+	e.Start(addr)
+}
+
+func echoHandler(c echo.Context) error {
+	req := c.Request()
+	resp := struct {
+		Request string
+		Header  http.Header
+	}{
+		Request: req.URL.String(),
+		Header:  req.Header,
+	}
+	return c.JSON(http.StatusOK, &resp)
 }
--- proxy.go
+++ proxy.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"sync"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func runProxy(ctx context.Context, wg *sync.WaitGroup) {
@@ -12,5 +18,39 @@ func runProxy(ctx context.Context, wg *sync.WaitGroup) {
 		wg.Done()
 	}()
 
-	log.Println("Proxy started")
+	e := echo.New()
+	e.HideBanner = true
+	go func() {
+		<-ctx.Done()
+		e.Shutdown(context.Background())
+	}()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "proxy")
+	})
+
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: mustParseUrl(fmt.Sprintf("http://localhost:%d", backendPort)),
+		},
+	}
+	g := e.Group("/proxy")
+	g.Use(middleware.ProxyWithConfig(middleware.ProxyConfig{
+		Balancer: middleware.NewRoundRobinBalancer(targets),
+		Rewrite: map[string]string{
+			"/proxy/*": "/$1",
+		},
+	}))
+
+	log.Println("Proxy starting up")
+	addr := fmt.Sprintf(":%d", proxyPort)
+	e.Start(addr)
+}
+
+func mustParseUrl(s string) *url.URL {
+	ret, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	return ret
 }
